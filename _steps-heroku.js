7d9f6c32d75bbfb17df67b54b575284cd62b85fa
akilinjamam <akilinjamam@gmail.comm>
2022-11-15 21:16:14 +0600

    file-updated

---
+++ _steps-heroku.js
@@ -0,0 +1,22 @@
+
+/* 
+one time for your computer:
+
+1. create heroku account
+2. verify account
+3. install heroku CLI
+4. heroku login
+
+
+For each Project One time:
+
+1. heroku create
+2. make sure your server code is pushed in github using:
+-git add  .
+-git commit  -m"updated"
+-git push
+
+3. finally git push heroku main
+
+
+*/
