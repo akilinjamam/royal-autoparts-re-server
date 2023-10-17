ec103d36586704aa57534b483ff3f5c7291169a2
akilinjamam <akilinjamam@gmail.comm>
2023-10-17 23:53:05 +0600

    file-updated

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.listen(port, () => {
 
 
 const uri = `${process.env.DB_URI}`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, serverSelectionTimeoutMS: 5000 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 //--------------------------------------------------------------------------------
 
